bdbc54065ae162deb1ce18de31a580fce0276efd
Shen GuoQing <yinweiyingzhe@126.com>
2023-11-27 10:51:41 +0800

    适配complex-request/axios

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { Request } from 'complex-request'
 import { RequestConfig } from 'complex-request/src/Request'
 import Rule from 'complex-request/src/Rule'
 
 class AxiosRequest extends Request{
   $request(requestConfig: RequestConfig, rule?: Rule, isRefresh?: boolean) {
-    return axios({
+    const axiosConfig: AxiosRequestConfig = {
       url: requestConfig.url,
       method: requestConfig.method,
       headers: requestConfig.headers,
@@ -13,13 +13,24 @@ class AxiosRequest extends Request{
       params: requestConfig.params,
       responseType: requestConfig.responseType,
       ...requestConfig.local
-    })
+    }
+    if (requestConfig.format) {
+      requestConfig.format(axiosConfig, rule, isRefresh)
+    }
+    return axios(axiosConfig)
   }
   $parseError(responseError: { response?: { status: number } }) {
     if (responseError.response) {
-      return this.status[responseError.response.status]
+      return {
+        type: 'server',
+        msg: this.status[responseError.response.status],
+        data: responseError
+      }
     } else {
-      return ''
+      return {
+        type: 'request',
+        data: responseError
+      }
     }
   }
 }
