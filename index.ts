f750b7c551570b71e7df0ff780707c812f1c9fb2
Shen GuoQing <yinweiyingzhe@126.com>
2023-11-27 09:43:07 +0800

    0.1.1-初始化

--- index.ts
+++ index.ts
@@ -1,4 +1,29 @@
+import axios from 'axios'
+import { Request } from 'complex-request'
+import { RequestConfig } from 'complex-request/src/Request'
+import Rule from 'complex-request/src/Rule'
+
+class AxiosRequest extends Request{
+  $request(requestConfig: RequestConfig, rule?: Rule, isRefresh?: boolean) {
+    return axios({
+      url: requestConfig.url,
+      method: requestConfig.method,
+      headers: requestConfig.headers,
+      data: requestConfig.data,
+      params: requestConfig.params,
+      responseType: requestConfig.responseType,
+      ...requestConfig.local
+    })
+  }
+  $parseError(responseError: { response?: { status: number } }) {
+    if (responseError.response) {
+      return this.status[responseError.response.status]
+    } else {
+      return ''
+    }
+  }
+}
 
 export {
-
+  AxiosRequest
 }
